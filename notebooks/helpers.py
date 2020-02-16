c20d37b2b98b5ed52b288b5d36333a35287f7d4b
Adrian Price-Whelan <adrian.prw@gmail.com>
2020-02-15 22:45:51 -0500

    helpers

--- notebooks/helpers.py
+++ notebooks/helpers.py
@@ -1,3 +1,5 @@
+import astropy.coordinates as coord
+import astropy.units as u
 from astropy.io import fits
 import healpy as hp
 import numpy as np
@@ -40,13 +42,49 @@ projs['gd1'] = hp.projector.MollweideProj(xsize=1024,
 projs['icrs-ortho'] = hp.projector.OrthographicProj(xsize=1024, 
                                                     rot=[180, 45, 0])
 
+def obj_func(ang, icrs, R):
+    R3 = rotation_matrix(ang, 'x')
+    rep = icrs.cartesian.transform(R3 @ R).represent_as(
+        coord.UnitSphericalRepresentation)
+    return np.sum(rep.lat.degree**2)
 
-def get_data(bass_file=None, decals_file=None):
+def get_roll(icrs, R, x0=0.):
+    res = minimize(obj_func, x0=x0, args=(icrs, R))
+    return coord.Angle(res.x[0] * u.deg)
+
+def get_origin(fr):
+    usph_origin = coord.UnitSphericalRepresentation(0*u.deg, 0*u.deg)
+    return fr.realize_frame(usph_origin).transform_to(coord.ICRS)
+
+def get_rot(fr, x0=0.):
+    origin = get_origin(fr)
+    usph_band  = coord.UnitSphericalRepresentation(np.random.uniform(0, 360, 10000)*u.deg, 
+                                                   np.random.uniform(-1, 1, size=10000)*u.deg)
+    band_icrs = fr.realize_frame(usph_band).transform_to(coord.ICRS)
+    
+    R1 = rotation_matrix(origin.ra, 'z')
+    R2 = rotation_matrix(-origin.dec, 'y')
+    Rtmp = R2 @ R1
+    
+    roll = get_roll(band_icrs, Rtmp, x0)
+    
+    return [origin.ra.degree, origin.dec.degree, roll.degree]
+
+from astropy.coordinates.matrix_utilities import rotation_matrix
+from scipy.optimize import minimize
+import gala.coordinates as gc
+projs['orp'] = hp.projector.MollweideProj(xsize=1024, 
+                                          rot=get_rot(gc.Orphan()))
+projs['pal5'] = hp.projector.MollweideProj(xsize=1024, 
+                                           rot=get_rot(gc.Pal5()))
+
+
+def get_data(bass_file=None, decals_file=None, bass_scale_factor=1.5):
     if bass_file is None and decals_file is None:
         raise ValueError()
     
     if bass_file is not None:
-        bass_cube = fits.getdata(bass_file, 0) / 1.5  # TODO: MAGIC NUMBER
+        bass_cube = fits.getdata(bass_file, 0) / bass_scale_factor
         cube_dm = fits.getdata(bass_file, 1)
         npix, nslice = bass_cube.shape
     else:
