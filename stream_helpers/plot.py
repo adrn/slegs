744fd3a63b575d9aefca338f1bc574d9d1ce6d9e
Adrian Price-Whelan <adrian.prw@gmail.com>
2020-02-17 14:57:44 -0500

    plotting helps

---
+++ stream_helpers/plot.py
@@ -0,0 +1,47 @@
+import astropy.units as u
+
+__all__ = ['StreamRidgeBuilder']
+
+
+class StreamRidgeBuilder:
+
+    def __init__(self, ax, proj):
+        self.ax = ax
+        self.reset()
+        fig = self.ax.figure
+
+        self.cids = {}
+        self.cids['mouse'] = fig.canvas.mpl_connect('button_press_event', self)
+        self.cids['key'] = fig.canvas.mpl_connect('key_press_event', self)
+
+    def reset(self):
+        self.line = None
+        self.xs = list()
+        self.ys = list()
+
+    def __call__(self, event):
+        if event.inaxes != self.ax:
+            return
+
+        self.xs.append(event.xdata)
+        self.ys.append(event.ydata)
+
+        if self.line is None:
+            self.line, = self.ax.plot(self.xs, self.ys, color='tab:red')
+        else:
+            self.line.set_data(self.xs, self.ys)
+
+        self.line.figure.canvas.draw()
+
+        if event.key == 'escape':
+            if len(self.xs) == 0:
+                self.reset()
+                return
+
+            endpoints = [self.proj.xy2ang(x, y, lonlat=True)
+                         for x, y in zip(self.xs, self.ys)] * u.deg
+            print("Track in ICRS coordinates (degrees):")
+            print(endpoints.to_value(u.deg).tolist())
+
+            self.line.set_data([], [])
+            self.reset()
