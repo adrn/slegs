a60b51284f2d7ae72dbc5eaacb5887ad94fec361
Adrian Price-Whelan <adrian.prw@gmail.com>
2020-05-29 16:29:16 -0400

    fix

--- stream_helpers/__init__.py
+++ stream_helpers/__init__.py
@@ -1,4 +1,4 @@
 from .coords import zyx_euler_from_endpoints, get_rot, rots
 from .maps import get_vscale, make_anim_frames
-from .data import get_data
+from .data import get_data, apwnorm, get_all_stitched
 from .plot import StreamRidgeBuilder
--- stream_helpers/data.py
+++ stream_helpers/data.py
@@ -2,35 +2,73 @@ from astropy.io import fits
 import numpy as np
 
 
-def get_data(bass_file=None, decals_file=None, bass_scale_factor=1.5):
-    if bass_file is None and decals_file is None:
-        raise ValueError()
+def get_data(bass_file, decals_file, stitch=False):
 
-    if bass_file is not None:
-        bass_cube = fits.getdata(bass_file, 0) / bass_scale_factor
-        cube_dm = fits.getdata(bass_file, 1)
-        npix, nslice = bass_cube.shape
+    cubes = {}
+
+    cubes['bass'] = fits.getdata(bass_file, 0)
+    distmods_bass = fits.getdata(bass_file, 1)
+
+    cubes['decals'] = fits.getdata(decals_file, 0)
+    distmods_decals = fits.getdata(decals_file, 1)
+
+    if not np.allclose(distmods_bass, distmods_decals):
+        raise ValueError("distmods don't match")
+
+    elif cubes['bass'].shape != cubes['decals'].shape:
+        raise ValueError("shapes don't match")
+
+    npix, nslice = cubes['bass'].shape
+
+    footprints = {}
+    for k in cubes.keys():
+        cubesum = np.sum(cubes[k], axis=1) / nslice
+        footprints[k] = cubesum != 0
+
+    if stitch:
+        stitched = get_all_stitched(cubes, footprints)
+        footprint = footprints['bass'] | footprints['decals']
+        return stitched, distmods_decals, footprint
+
     else:
-        bass_cube = None
+        return cubes, distmods_decals, footprints
 
-    if decals_file is not None:
-        decals_cube = fits.getdata(decals_file, 0)
-        cube_dm = fits.getdata(decals_file, 1)
-        npix, nslice = decals_cube.shape
-    else:
-        decals_cube = None
 
-    if decals_cube is None:
-        decals_cube = np.zeros_like(bass_cube)
-    elif bass_cube is None:
-        bass_cube = np.zeros_like(decals_cube)
+def stitch_slice(bass, decals, overlap, bass_footprint,
+                 min_bass=0, min_decals=0):
+    mask = (bass > min_bass) & (decals > min_decals) & overlap
+    poly = np.poly1d(np.polyfit(bass[mask], decals[mask],
+                                deg=1))
 
-    tmp_bass = bass_cube.copy()
-    tmp_bass[decals_cube != 0] = 0.
+    overlap = overlap & (bass > 0) & (decals > 0)
 
-    both = decals_cube + tmp_bass
+    new_bass = poly(bass)
+    stitched = decals.copy()
+    stitched[bass_footprint] += new_bass[bass_footprint]
+    stitched[overlap] *= 0.5
 
-    cubesum = np.sum(both, axis=1) / nslice
-    footprint_mask = cubesum != 0
+    return stitched
 
-    return both, cube_dm, footprint_mask
+
+def get_all_stitched(cubes, footprints):
+    overlap = (footprints['bass'] * footprints['decals']).astype(bool)
+
+    npix, nslice = cubes['bass'].shape
+
+    all_stitched = []
+    for i in range(nslice):
+        stitched = stitch_slice(cubes['bass'][:, i], cubes['decals'][:, i],
+                                overlap, footprints['bass'],
+                                min_bass=4)
+        all_stitched.append(stitched)
+    all_stitched = np.stack(all_stitched, axis=1)
+
+    return all_stitched
+
+
+def apwnorm(x, min=None, max=None):
+    if min is None:
+        min = np.nanmin(x[~np.isinf(x)])
+    if max is None:
+        max = np.nanmax(x[~np.isinf(x)])
+    return np.clip((x - min) / (max - min), 0, 1)
